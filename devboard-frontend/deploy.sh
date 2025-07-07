c48ef6be8c5a813f4631e5ec01729ff175ac573d
zzho0113 <zzho0113.student.monash.edu>
2025-07-08 00:30:24 +1000

    Unify deploy workflow and script using nohup + nginx

--- devboard-frontend/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "📦 Building frontend..."
-npm install
-npm run build
-
-echo "🚀 Deploying to Nginx directory..."
-rm -rf ~/devboard-frontend-dist/*
-cp -r dist/* ~/devboard-frontend-dist/
