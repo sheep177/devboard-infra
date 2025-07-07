29645db374871a2150ed47664eba49cda21917ec
zzho0113 <zzho0113.student.monash.edu>
2025-07-07 15:53:25 +1000

    startup CORS patch print to confirm redeploy

--- devboard-backend/src/main/java/com/devboard/DevBoardApplication.java
+++ devboard-backend/src/main/java/com/devboard/DevBoardApplication.java
@@ -8,6 +8,8 @@ public class DevBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevBoardApplication.class, args);
+		System.out.println("🟢 Application started with CORS patch");
+
 	}
 
 }
