49574c7bcba3ce15ee131f9e9308ef5a114decf3
zzho0113 <zzho0113.student.monash.edu>
2025-07-09 16:53:12 +1000

    fix incomplete chunke

--- devboard-backend/src/main/java/com/devboard/security/JwtAuthenticationFilter.java
+++ devboard-backend/src/main/java/com/devboard/security/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -51,10 +54,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             System.out.println("🔍 Loaded user from DB: " + userDetails.getUsername());
 
             if (jwtUtil.isTokenValid(jwt, userDetails)) {
-                System.out.println("✅ Token is valid");//1
+                System.out.println("✅ Token is valid");
+
+                String role = jwtUtil.extractRole(jwt);
+                List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
 
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+                        userDetails, null, authorities);
 
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
@@ -62,6 +68,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 System.out.println("❌ Token is invalid");
             }
         }
+
 
         filterChain.doFilter(request, response);
     }}
